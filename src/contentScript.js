0ea25e98611881a0bf739561c03d9ed1e43b3d8a
Sergio Pérez <sergi9p@gmail.com>
2019-02-10 21:00:28 +0100

    Hotfix

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,7 +2,6 @@ import { getPublicationByName, watched } from "./utils/store";
 
 (function() {
   let image;
-  let href;
   let chapter = window.location.pathname;
   chapter = chapter.substring(1, chapter.length - 1);
   if (!chapter.startsWith("lista")) {
@@ -40,36 +39,36 @@ import { getPublicationByName, watched } from "./utils/store";
   }
 
   $("a.newlist-epi__title").each(function() {
-    href = $(this).attr("href");
-    chapter = href.substring(22, href.length - 1);
-    getPublicationByName(chapter).then(res => {
+    const href = $(this).attr("href");
+    const listChapter = href.substring(22, href.length - 1);
+    getPublicationByName(listChapter).then(res => {
       if (!jQuery.isEmptyObject(res)) {
         image = browser.extension.getURL("images/eye-solid.svg");
         $(this).before(
           "<div class='watched-list' id='" +
-            chapter +
+            listChapter +
             "'><img src=" +
             image +
             " alt='" +
-            chapter +
+            listChapter +
             "'></div>"
         );
-        $("div#" + chapter).click(function() {
-          watched(chapter);
+        $("div#" + listChapter).click(function() {
+          watched(listChapter);
         });
       } else {
         image = browser.extension.getURL("images/eye-slash-solid.svg");
         $(this).before(
           "<div class='watched-list' id='" +
-            chapter +
+            listChapter +
             "'><img src='" +
             image +
             "' alt='" +
-            chapter +
+            listChapter +
             "'></div>"
         );
-        $("div#" + chapter).click(function() {
-          watched(chapter);
+        $("div#" + listChapter).click(function() {
+          watched(listChapter);
         });
       }
     });
